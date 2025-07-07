35ba1242ad31f3de8d9710ae86e5356ec2061cb9
AliasgarKachwala17 <aliasgar.kachwala4604@gmail.com>
2025-07-07 16:37:13 +0530

    Cors error solved

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -5,6 +5,8 @@ from .views import CampaignView
 urlpatterns = [
     # list and create
     path('campaign/nt/', CampaignView.as_view(), name='campaign-list'),
+    path('campaign/', CampaignView.as_view(), name='campaign-list'),
     # retrieve, update, delete
     path('campaign/<int:pk>/nt/', CampaignView.as_view(), name='campaign-detail'),
+    path('campaign/<int:pk>/', CampaignView.as_view(), name='campaign-detail'),
 ]
